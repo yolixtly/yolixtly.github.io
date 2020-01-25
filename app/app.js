4106dfdd396990997bd6ac644e988103cdfaf930
yolixtly <yolixtly_13@hotmail.com>
2020-01-24 16:50:54 -0800

    update Header section

--- app/app.js
+++ app/app.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
-  $(".navbar-nav li a").click(function(event) {
-    $(".navbar-collapse").collapse('hide');
+  // Navbar Collapse for Responsive Design
+  $('.navbar-nav li a').click(function (event) {
+    $('.navbar-collapse').collapse('hide');
   });
+
+  // Highlight Skills on Page Load
+  const skills = $('.main-btn');
+  let start = 0;
+  let end = 500;
+  let increment = end;
+  for (let i = 0; i < skills.length; i++) {
+    setTimeout(() => {
+      skills[i].classList.add('hover');
+    }, start);
+
+    setTimeout(() => {
+      skills[i].classList.remove('hover');
+    }, end);
+    start = end;
+    end = end + increment;
+  }
+
+  // Market opportunities
+  setTimeout(() => {
+    $('#relocation').removeClass('relocation');
+    $('h2.relocation').addClass('hover');
+  }, 2500);
+
+  setTimeout(() => {
+    $('#relocation').addClass('relocation');
+    $('#relocation').removeClass('hover');
+  }, 4000);
 });
